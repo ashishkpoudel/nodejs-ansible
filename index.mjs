105adb118f8b911d9cea17e7e19f4a2444cebaac
Ashish K. Poudel <routeasis@gmail.com>
2023-06-24 11:45:08 +0545

    configure to use dynamic ec2 inventory

--- index.mjs
+++ index.mjs
@@ -3,10 +3,10 @@ import express from "express";
 const app = express();
 const port = 3100;
 
-app.get('/', async (_req, res) => {
-  res.send('New feature added....');
+app.get('/', (_req, res) => {
+  res.send('healthy...')
 })
 
 app.listen(port, () => {
   console.log(`app running on: ${port}`);
-});
+})
